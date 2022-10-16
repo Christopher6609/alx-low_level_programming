3caee75679949851da8adc937bb4da70185159c7
Chris <odeniyichristopher7@gmail.com>
2022-10-16 13:12:36 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+/**
+
+ * main - Entry point
+
+ * 
+
+ * Description: 'the program's description'
+
+ * 
+
+ * Return: Always 0 (Success)
+
+ */
+
+
 int main(void)
 {
 printf("size of a char: %lu byte(s)\n", sizeof(char));
